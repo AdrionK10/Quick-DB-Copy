75694578da35d36259dcf134cbf3f415e917ee71
HuxleysFather <85905898+HuxleysFather@users.noreply.github.com>
2022-01-20 15:01:51 -0800

    update to db copy for new placement of local thrive db

--- QDBS.bat
+++ QDBS.bat
@@ -1,6 +1,6 @@
 @echo off
 
 IF NOT DEFINED dirIDNetwork set "dirID=\\192.168.0.10\Thrive"
-if not exist "C:\Users\%username%\Desktop\ThriveDBCopy\" mkdir C:\Users\%username%\Desktop\ThriveDBCopy\"
-IF NOT DEFINED destinationFolder set "destinationFolder= C:\Users\%username%\Desktop\ThriveDBCopy\"
+if not exist "C:\inetpub\wwwroot\ThriveDBCopy\" mkdir C:\inetpub\wwwroot\ThriveDBCopy\"
+IF NOT DEFINED destinationFolder set "destinationFolder= C:\inetpub\wwwroot\ThriveDBCopy\"
 robocopy %dirID%  %destinationFolder% /MIR 
